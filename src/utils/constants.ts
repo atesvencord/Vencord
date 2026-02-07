f6a6ff15c1d5e3279bd565cb8917305a2eeb093c
atesvencord <gecgec063@gmail.com>
2026-02-07 03:24:39 +0300

    add dev constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    ates: {
+        name: "ates",
+        id: 699989491128991775n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
